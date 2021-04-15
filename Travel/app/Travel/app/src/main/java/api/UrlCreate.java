1903c02a9b350212273664132e4244eab62f62db
HyeRim-Choi <hyerim252@gmail.com>
2021-04-15 16:22:50 +0900

    Package 여행 final

--- Travel/app/Travel/app/src/main/java/api/UrlCreate.java
+++ Travel/app/Travel/app/src/main/java/api/UrlCreate.java
@@ -2,7 +2,7 @@ package api;
 
 
 public class UrlCreate {
-    private static final String ip = "13.124.66.133";
+    private static final String ip = "";
     private static final int port = 3001;
 
     public static String getUrl(int chk, String info) {
